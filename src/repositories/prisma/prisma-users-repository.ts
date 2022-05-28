94598d1e6aa3c43751313fbd96fb9f2632f5d74c
lvneto <lauro.neto@irriga.net>
2022-05-28 13:36:20 -0300

    get user by cnpj or cpf

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,20 +3,13 @@ import { UsersRepository } from '../users.repository';
 
 export class PrismaUsersRepository implements UsersRepository {
 
-  async findUsers(skip: number, take: number): Promise<any> {
-    return await prisma.users.findMany({
-      skip: skip || 0,
-      take: take || 30      
-    })
-  } 
-
   async findUser (skip: number, take: number, cnpj: string, cpf: string): Promise<any> {
     return await prisma.users.findMany({
       where: {       
-        OR:  [{ cnpj }, { cpf }]     
-      }, 
+        OR:  [{ cnpj }, { cpf }]           
+      },    
       skip: skip || 0,
-      take: take || 10  
+      take: take || 100  
     })
   }
 
