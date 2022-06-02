351571c17ee631255027192228681c2dfd6c0dc6
lvneto <lauro.neto@irriga.net>
2022-06-02 20:43:16 -0300

    create middleware to just a parameter

--- src/middleware/users.middleware.ts
+++ src/middleware/users.middleware.ts
@@ -2,17 +2,11 @@ import { Response } from 'express';
 
 export const sanitizeUsersParameters = async(request: any, response: Response, next: any): Promise<any> => {
 
-  if (request.query.cnpj !== undefined) {
-    request.query.cnpj = request.query.cnpj.replaceAll('-', '')
-    request.query.cnpj = request.query.cnpj.replaceAll('.', '')
-    request.query.cnpj = request.query.cnpj.replaceAll('/', '')
-  }
-
-  if (request.query.cpf !== undefined) {
-    request.query.cpf = request.query.cpf.replaceAll('-', '')
-    request.query.cpf = request.query.cpf.replaceAll('.', '')
-    request.query.cpf = request.query.cpf.replaceAll('/', '')
-  }
+  if (request.query.doc !== undefined) {
+    request.query.doc = request.query.doc.replaceAll('-', '')
+    request.query.doc = request.query.doc.replaceAll('.', '')
+    request.query.doc = request.query.doc.replaceAll('/', '')
+  } 
    
  next()
 }
